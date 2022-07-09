a7b9ffe3bb9bc1fc1dd5629d65359fba6137f8e4
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-07-09 11:14:05 -0300

    feat: list components in App and add logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
+
 export function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
