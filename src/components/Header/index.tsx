ae440eb4956d82ba4b9714cd493956b6660b92a1
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-07-29 11:50:54 -0300

    chore: corrige spaces

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,8 @@
-import { HeaderContainer } from './styles'
-import { Timer, Scroll } from 'phosphor-react'
-import logoIgnite from '../../assets/logo-ignite.svg'
 import { NavLink } from 'react-router-dom'
+import { Timer, Scroll } from 'phosphor-react'
+import { HeaderContainer } from './styles'
+
+import logoIgnite from '../../assets/logo-ignite.svg'
 
 export function Header() {
   return (
